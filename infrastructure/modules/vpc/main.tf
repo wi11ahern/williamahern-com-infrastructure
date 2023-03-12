d86e2c47ba42a00dcad9c16956541355c59c2ca7
William Ahern <willdahern@gmail.com>
2023-03-12 11:21:54 -0400

    Add NAT gateway

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,27 +1,44 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags = merge(local.common_tags, { "Name": "${var.project_name}-vpc"})
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-VPC"})
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags = merge(local.common_tags, { "Name": "${var.project_name}-public"})
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-Public-Subnet"})
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 
-  tags = merge(local.common_tags, { "Name": "${var.project_name}-private"})
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-Private-Subnet"})
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge(local.common_tags, { "Name": "${var.project_name}-igw"})
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-IGW"})
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc      = true
+
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-NAT-Gateway-IP"})
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.internet_gateway]
+
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-NAT-Gateway"})
 }
 
 resource "aws_route_table" "public_route_table" {
@@ -32,10 +49,26 @@ resource "aws_route_table" "public_route_table" {
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags = merge(local.common_tags, { "Name": "${var.project_name}-route-table"})
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-Public-Route-Table"})
 }
 
 resource "aws_route_table_association" "public_subnet_rt_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = merge(local.common_tags, { "Name": "${var.project_name}-Private-Route-Table"})
+}
+
+resource "aws_route_table_association" "private_subnet_rt_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
